88869a1edc8a111c725e0aab569723d0521b5c5d
ShadowMiner <ilinsky.danila@yandex.ru>
2016-08-17 01:41:01 +0300

    локализация и стаки
    локализовал скиллы фантомы и сделал чтобы стаки при активировании
    пассивки со спины увеличивались на 1 а если модификатора нету то
    добавляется 2 стака

--- scripts/vscripts/heroes/PhantomAssassin/phantom_assassin_back_stab.lua
+++ scripts/vscripts/heroes/PhantomAssassin/phantom_assassin_back_stab.lua
@@ -104,7 +104,7 @@ function modifier_phantom_assassin_back_stab_passive:OnAttackLanded( params )
 				ability:StartCooldown(ability:GetCooldown(ability:GetLevel()))
 				target:AddNewModifier(caster,ability,"modifier_phantom_assassin_back_stab_b",{duration = ability:GetLevelSpecialValueFor("duration_b", ability:GetLevel())})
 				self:GetParent():AddNewModifier(caster,ability,"modifier_phantom_assassin_back_stab_atk_speed",{})
-				caster:SetModifierStackCount("modifier_phantom_assassin_back_stab_atk_speed",caster,2)
+				caster:SetModifierStackCount("modifier_phantom_assassin_back_stab_atk_speed",caster, caster:GetModifierStackCount("modifier_phantom_assassin_back_stab_atk_speed", self:GetAbility()) + 2)
 			end
 		end
 
@@ -179,7 +179,7 @@ function modifier_phantom_assassin_back_stab_b:CheckState()
 end
 
 function modifier_phantom_assassin_back_stab_b:DeclareFunctions()
-	local funcs = { MODIFIER_PROPERTY_OVERRIDE_ANIMATION, MODIFIER_EVENT_ON_ATTACK_LANDED }
+	local funcs = { MODIFIER_PROPERTY_OVERRIDE_ANIMATION }
 	return funcs
 end
 
