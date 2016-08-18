9bd9cdb00efd07fad3f5900a2cde5e3c503824af
ShadowMiner <ilinsky.danila@yandex.ru>
2016-08-18 21:47:42 +0300

    интервал

---
+++ scripts/vscripts/items/item_attribute_boost_strength.lua
@@ -0,0 +1,12 @@
+function Base(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local str = "modifier_attribute_boost_strength_stacks"
+
+	if not caster:HasModifier(str) then
+		ability:ApplyDataDrivenModifier(caster, caster, str, {})
+	elseif caster:HasModifier(str) then
+		local stacks = caster:GetModifierStackCount(str, caster)
+		caster:SetModifierStackCount(str, caster, stacks + 1)
+	end
+end
