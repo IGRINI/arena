0b13bbe7c519f8aa9ddf41ff756cbb41f145af63
ShadowMiner <ilinsky.danila@yandex.ru>
2016-08-19 17:18:58 +0300

    барабаны
    доработал и пофиксил

--- scripts/vscripts/items/item_drums_of_vengefence.lua
+++ scripts/vscripts/items/item_drums_of_vengefence.lua
@@ -2,17 +2,16 @@ if item_drums_of_vengefence == nil then
 	item_drums_of_vengefence = class({})
 end
 
-LinkLuaModifier("modifier_asara_ring","items/item_asara_ring.lua",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_drums_of_vengefence_passive","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_asara_ring_aura","items/item_asara_ring.lua",LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_drums_of_vengefence_passive_aura_emmiter","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_drums_of_vengefence_passive_aura","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_drums_of_vengefence","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_drums_of_vengefence_negative","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_drums_of_vengefence_aura","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_drums_of_vengefence_aura_negative","items/item_drums_of_vengefence.lua",LUA_MODIFIER_MOTION_NONE)
 
 function item_drums_of_vengefence:GetIntrinsicModifierName()
-	local mods = { "modifier_asara_ring", "modifier_drums_of_vengefence_passive" }
-    return mods 
+    return "modifier_drums_of_vengefence_passive_aura_emmiter"
 end
 
 function item_drums_of_vengefence:OnSpellStart()
@@ -20,8 +19,9 @@ function item_drums_of_vengefence:OnSpellStart()
 	if charges <= 0 then
 		return nil
 	end
-	self:GetAbility():SetCurrentCharges(charges = charges - 1)
+	self:SetCurrentCharges(charges - 1)
 	self:GetCaster():AddNewModifier(self:GetCaster(),self,"modifier_drums_of_vengefence",{duration = 5.5})
+	self:GetCaster():AddNewModifier(self:GetCaster(),self,"modifier_drums_of_vengefence_negative",{duration = 5.5})
 end
 
 ---------------------------------------------------------------------
@@ -34,12 +34,12 @@ function modifier_drums_of_vengefence:IsAura()
 	return true
 end
 
-function modifier_drums_of_vengefence:GetTexture()
-    return "item_drums_of_vengefence"
-end
-
 function modifier_drums_of_vengefence:IsPurgable()
     return false
+end
+
+function modifier_drums_of_vengefence:IsHidden()
+	return true
 end
 
 function modifier_drums_of_vengefence:GetAuraRadius()
@@ -68,33 +68,33 @@ if modifier_drums_of_vengefence_aura == nil then
 	modifier_drums_of_vengefence_aura = class({})
 end
 
-function modifier_drums_of_vengefence_aura:GetEffectName()
-    return "particles/units/heroes/hero_huskar/loot_greevil_ambient_rare_sparks.vpcf"
-end
-
-function modifier_drums_of_vengefence:GetEffectAttachType()
+function modifier_drums_of_vengefence_aura:GetEffectAttachType()
     return PATTACH_ABSORIGIN_FOLLOW
 end
 
-function modifier_asara_ring_aura:GetEffectName()
+function modifier_drums_of_vengefence_aura:GetEffectName()
     return "particles/units/unit_greevil/huskar_berserker_blood_hero_effect.vpcf"
 end
 
+function modifier_drums_of_vengefence_aura:GetTexture()
+    return "item_drums_of_vengefence"
+end
+
 function modifier_drums_of_vengefence_aura:IsHidden()
-	return true
+	return false
 end
 
 function modifier_drums_of_vengefence_aura:DeclareFunctions()
-	local funcs = { MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS, MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT }
+	local funcs = { MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT, MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE }
 	return funcs
 end
 
-function modifier_drums_of_vengefence_aura:GetModifierConstantHealthRegen(params)
-	return 3.35
+function modifier_drums_of_vengefence_aura:GetModifierAttackSpeedBonus_Constant(params)
+	return 30
 end
 
-function modifier_drums_of_vengefence_aura:GetModifierPhysicalArmorBonus(params)
-	return 3.4
+function modifier_drums_of_vengefence_aura:GetModifierMoveSpeedBonus_Percentage(params)
+	return 30
 end
 
 ---------------------------------------------------------------------
@@ -107,16 +107,8 @@ function modifier_drums_of_vengefence_negative:IsAura()
 	return true
 end
 
-function modifier_drums_of_vengefence_negative:GetTexture()
-    return "item_asara_ring"
-end
-
-function modifier_drums_of_vengefence_negative:GetEffectAttachType()
-    return PATTACH_ABSORIGIN_FOLLOW
-end
-
-function modifier_drums_of_vengefence_negative:GetEffectName()
-    return "particles/units/unit_greevil/loot_greevil_death_cloud.vpcf"
+function modifier_drums_of_vengefence_negative:IsHidden()
+	return true
 end
 
 function modifier_drums_of_vengefence_negative:IsPurgable()
@@ -124,15 +116,11 @@ function modifier_drums_of_vengefence_negative:IsPurgable()
 end
 
 function modifier_drums_of_vengefence_negative:GetAuraRadius()
-    return self:GetAbility():GetSpecialValueFor("radius")
+    return 1020
 end
 
 function modifier_drums_of_vengefence_negative:GetModifierAura()
-    return "modifier_asara_ring_aura"
-end
-
-function modifier_drums_of_vengefence_negative:GetEffectAttachType()
-    return PATTACH_ABSORIGIN_FOLLOW
+    return "modifier_drums_of_vengefence_aura_negative"
 end
    
 function modifier_drums_of_vengefence_negative:GetAuraSearchTeam()
@@ -153,12 +141,20 @@ if modifier_drums_of_vengefence_aura_negative == nil then
 	modifier_drums_of_vengefence_aura_negative = class({})
 end
 
+function modifier_drums_of_vengefence_aura_negative:GetTexture()
+    return "item_drums_of_vengefence"
+end
+
 function modifier_drums_of_vengefence_aura_negative:GetEffectName()
-    return "particles/units/unit_greevil/loot_greevil_ambient_rare_sparks.vpcf"
+    return "particles/units/unit_greevil/loot_greevil_death_cloud.vpcf"
+end
+
+function modifier_drums_of_vengefence_aura_negative:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
 end
 
 function modifier_drums_of_vengefence_aura_negative:IsHidden()
-	return true
+	return false
 end
 
 function modifier_drums_of_vengefence_aura_negative:DeclareFunctions()
@@ -174,49 +170,124 @@ function modifier_drums_of_vengefence_aura_negative:GetModifierAttackSpeedBonus_
 	return -25
 end
 
----------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 
-if modifier_drums_of_vengefence_passive == nil then
-	modifier_drums_of_vengefence_passive = class({})
+if modifier_drums_of_vengefence_passive_aura_emmiter == nil then
+	modifier_drums_of_vengefence_passive_aura_emmiter = class({})
 end
 
-function modifier_drums_of_vengefence_passive:IsHidden()
+function modifier_drums_of_vengefence_passive_aura_emmiter:IsAura()
 	return true
 end
 
-function modifier_drums_of_vengefence_passive:DeclareFunctions()
+function modifier_drums_of_vengefence_passive_aura_emmiter:IsHidden()
+	return true
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:DeclareFunctions()
 	local funcs = { MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS, MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT, MODIFIER_PROPERTY_STATS_STRENGTH_BONUS, MODIFIER_PROPERTY_STATS_AGILITY_BONUS, MODIFIER_PROPERTY_STATS_INTELLECT_BONUS, MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE, MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT, MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE }
 	return funcs
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierConstantHealthRegen(params)
-	return 3.35
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierConstantHealthRegen(params)
+	return 3.55
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierPhysicalArmorBonus(params)
-	return 3.4
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierPhysicalArmorBonus(params)
+	return 3
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierPreAttack_BonusDamage(params)
-	return 3.4
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierPreAttack_BonusDamage(params)
+	return 3
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierBonusStats_Strength(params)
-	return 6.5
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierBonusStats_Strength(params)
+	return 6
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierBonusStats_Agility(params)
-	return 3.2
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierBonusStats_Agility(params)
+	return 3
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierBonusStats_Intellect(params)
-	return 9.9
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierBonusStats_Intellect(params)
+	return 9
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierAttackSpeedBonus_Constant(params)
-	return 30
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierAttackSpeedBonus_Constant(params)
+	return 5
 end
 
-function modifier_drums_of_vengefence_passive:GetModifierMoveSpeedBonus_Percentage(params)
-	return -25
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierMoveSpeedBonus_Percentage(params)
+	return 5
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:IsPurgable()
+    return false
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetAuraRadius()
+    return 1020
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetModifierAura()
+    return "modifier_drums_of_vengefence_passive_aura"
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+   
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_FRIENDLY
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO
+end
+
+function modifier_drums_of_vengefence_passive_aura_emmiter:GetAuraDuration()
+    return 0.5
+end
+
+---------------------------------------------------------------------
+
+if modifier_drums_of_vengefence_passive_aura == nil then
+	modifier_drums_of_vengefence_passive_aura = class({})
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetTexture()
+    return "item_drums_of_vengefence"
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetEffectName()
+    return "particles/units/unit_greevil/loot_greevil_ambient_rare_sparks.vpcfx"
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_drums_of_vengefence_passive_aura:IsHidden()
+	return false
+end
+
+function modifier_drums_of_vengefence_passive_aura:DeclareFunctions()
+	local funcs = { MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT, MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE, MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS, MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT }
+	return funcs
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetModifierConstantHealthRegen(params)
+	return 3.55
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetModifierPhysicalArmorBonus(params)
+	return 3
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetModifierAttackSpeedBonus_Constant(params)
+	return 5
+end
+
+function modifier_drums_of_vengefence_passive_aura:GetModifierMoveSpeedBonus_Percentage(params)
+	return 5
 end
