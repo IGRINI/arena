b3eb96b2cd0c709e72de427d372d6b81d187b5cf
ShadowMiner <ilinsky.danila@yandex.ru>
2016-08-26 22:07:30 +0300

    some fixes

--- scripts/vscripts/heroes/PhantomAssassin/phantom_assassin_oppression.lua
+++ scripts/vscripts/heroes/PhantomAssassin/phantom_assassin_oppression.lua
@@ -1,22 +1,69 @@
-function Base(keys)
-	local target = keys.target()
-	local ability = keys.ability()
-	local kills = target:GetKills()
-	local caster = keys.caster()
+require('libraries/timers')
+
+if phantom_assassin_oppression == nil then
+	phantom_assassin_oppression = class({})
+end
+
+LinkLuaModifier("modifier_pa_oppression","heroes/PhantomAssassin/phantom_assassin_oppression.lua",LUA_MODIFIER_MOTION_NONE)
+
+function phantom_assassin_oppression:OnSpellStart(  )
+	local target = self:GetCursorTarget()
+	local caster = self:GetCaster()
+	local kills = target:GetPlayerID():GetKills()
 	local deaths = target:GetDeaths()
-	local dpk = keys.dpk
-	local dpd = keys.dpd
-	local duration = keys.duration
+	local dpd = kills * self:GetLevelSpecialValueFor("dpd",self:GetLevel())
+	local dpk = deaths * self:GetLevelSpecialValueFor("dpk",self:GetLevel())
+	local damageperkill = (dpd * dpk) / self:GetLevelSpecialValueFor("duration",self:GetLevel())
+	target:AddNewModifier(self:GetCaster(),self,"modifier_pa_oppression",{duration = self:GetLevelSpecialValueFor("duration",self:GetLevel())})
+	print(kills)
+end
 
-	if kills == nil then
-	killls = 1
-	end
+------------------------------------------------------------------------------------------------------------------------------------------------
 
-	if deaths == nil then
-	deaths = 1
-	end
+if modifier_pa_oppression == nil then
+	modifier_pa_oppression = class({})
+end
 
-	local damage = (kills * dpk / (deaths * dpd) / duration)
-	local damage_table = { victim = target,attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL }
-	ApplyDamage(damage_table)
+function modifier_pa_oppression:GetAttributes(  )
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+function modifier_pa_oppression:IsDebuff(  )
+	return true
+end
+
+function modifier_pa_oppression:GetTexture(  )
+	return self:GetAbility()
+end
+
+function modifier_pa_oppression:OnCreated(  )
+	local target = self:GetParent()
+	local caster = self:GetAbility():GetCaster()
+	local kills = target:GetPlayerID():GetKills()
+	local deaths = target:GetDeaths()
+	local dpd = kills * self:GetLevelSpecialValueFor("dpd",self:GetLevel())
+	local dpk = deaths * self:GetLevelSpecialValueFor("dpk",self:GetLevel())
+	local damageperkill = (dpd * dpk) / self:GetLevelSpecialValueFor("duration",self:GetLevel())
+	local damage_timer = Timers:CreateTimer(0,function()
+		ApplyDamage({victim = target, attacker = caster, damage = damageperkill, damage_type = DAMAGE_TYPE_MAGICAL})
+	return 0.5
+	end)
+end
+
+function modifier_pa_oppression:OnRefresh(  )
+	local target = self:GetParent()
+	local caster = self:GetAbility():GetCaster()
+	local kills = target:GetPlayerID():GetKills()
+	local deaths = target:GetDeaths()
+	local dpd = kills * self:GetLevelSpecialValueFor("dpd",self:GetLevel())
+	local dpk = deaths * self:GetLevelSpecialValueFor("dpk",self:GetLevel())
+	local damageperkill = (dpd * dpk) / self:GetLevelSpecialValueFor("duration",self:GetLevel())
+	local damage_timer = Timers:CreateTimer(0,function()
+		ApplyDamage({victim = target, attacker = caster, damage = damageperkill, damage_type = DAMAGE_TYPE_MAGICAL})
+	return 0.5
+	end)
+end
+
+function modifier_pa_oppression:OnDestroy(  )
+	Timers:RemoveTimer(damage_timer)
 end
